6ab1647e932c0f1c62b351b72a8237c7331094be
Home-Zsolt <zsolt@Home-Zsolt>
2026-08-29 11:14:09 +0200

    Release Virtual Switch v0.3.0008

--- custom_components/virtual_switch/_version.py
+++ custom_components/virtual_switch/_version.py
@@ -1,5 +1,5 @@
 """Generated version metadata. Do not edit manually."""
 
-__version__ = "0.2.0008"
-__version_tuple__ = (0, 2, 8)
-__version_time__ = "2026-08-29T09:48:40+02:00"
+__version__ = "0.3.0008"
+__version_tuple__ = (0, 3, 8)
+__version_time__ = "2026-08-29T11:13:36+02:00"
